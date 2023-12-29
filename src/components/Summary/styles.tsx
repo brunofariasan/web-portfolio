0fdc620dcccada03e3438469531c740c24921fb7
Bruno dos Santos Faria <bruno.faria@compasso.com.br>
2023-12-29 17:48:42 -0300

    Add section Summary

---
+++ src/components/Summary/styles.tsx
@@ -0,0 +1,77 @@
+import styled, { css } from "styled-components";
+import { theme } from "@/styles/theme";
+
+export const Container = styled.div`
+  min-height: 800px;
+  background-color: ${theme.colors.deepSlateBlue};
+`;
+
+export const CardSection = styled.div`
+  padding: 228.64px 0 102.7px;
+  padding-left: clamp(0.1rem, 1.8rem + 15.9vw, 36.2rem);
+  padding-right: clamp(0.1rem, 1.8rem + 15.9vw, 36.2rem);
+
+  @media (max-width: 1200px) {
+    padding: 135.64px 0 102.7px;
+    padding-left: clamp(0.1rem, 0.8rem + 8.9vw, 36.2rem);
+    padding-right: clamp(0.1rem, 0.8rem + 8.9vw, 36.2rem);
+  }
+`;
+
+export const TextSection = styled.div`
+  padding-left: clamp(0.1rem, 1.8rem + 15.9vw, 36.2rem);
+  padding-right: clamp(0.1rem, 1.8rem + 15.9vw, 36.2rem);
+  display: flex;
+  gap: 35px;
+  justify-content: center;
+
+  @media (max-width: ${theme.breakpoints.lg}) {
+    flex-direction: column;
+    padding-bottom: clamp(0.1rem, 1.8rem + 15.9vw, 36.2rem);
+  }
+`;
+
+export const FlexEnd = styled.div`
+  display: flex;
+  text-align: end;
+
+  @media (max-width: ${theme.breakpoints.lg}) {
+    max-width: 100%;
+    text-align: center;
+  }
+`;
+
+export const Text = styled.div`
+  max-width: 700px;
+
+  @media (max-width: ${theme.breakpoints.lg}) {
+    text-align: center;
+    max-width: 100%;
+  }
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 35px;
+
+  @media (max-width: 900px) {
+    div {
+      flex: 1 0 calc(50% - 12.5px);
+    }
+  }
+  @media (max-width: 550px) {
+    justify-content: center;
+  }
+`;
+
+export const FlexItem = styled.div`
+  flex: 0 0 calc(50% - 25px);
+  box-sizing: border-box;
+  max-width: 200px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -1,10 +1,14 @@
 import Banner from "@/components/Banner";
+import SliderCard from "@/components/SliderCard";
+import Summary from "@/components/Summary";
 import React from "react";
 
 export const Home: React.FC = ({}) => {
   return (
     <>
       <Banner />
+      {/* <SliderCard /> */}
+      <Summary />
     </>
   );
 };
