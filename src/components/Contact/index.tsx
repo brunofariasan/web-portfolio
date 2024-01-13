9a344bcd4db0ba6b2717df43eedde3dc4e3d56b1
Bruno dos Santos Faria <bruno.faria@compasso.com.br>
2024-01-12 23:54:51 -0300

    feat: Added email sending functionality in the contact section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import Flex from "../micro/Flex";
 import Text from "../micro/Text";
 import { TEXT } from "./constants";
 import * as S from "./styles";
+import emailjs from "@emailjs/browser";
+import Spinner from "../micro/Spinner/Spinner";
+
 export default function Contact() {
-  const handleSubmit = (e: React.FormEvent) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function sendEmail(e: any) {
     e.preventDefault();
-    console.log("Mensagem enviada!");
-  };
+    const templateParams = {
+      name: name,
+      email: email,
+      message: message,
+    };
+    if (name === "" || email === "" || message === "") {
+      setIsLoading(false);
+      return;
+    } else {
+      setIsLoading(true);
+    }
+
+    emailjs
+      .send(
+        "serviceGmail",
+        "template_3h3cuma",
+        templateParams,
+        "X2vukpea5orjw7kwk"
+      )
+      .then(
+        (response) => {
+          setName("");
+          setEmail("");
+          setMessage("");
+          setIsLoading(false);
+        },
+        (err) => {}
+      );
+  }
+
   return (
     <S.Container>
+      {isLoading && <Spinner />}
       <S.Section>
         <Flex flexDirection="column">
           <Text variant="contact-section-title-description">
@@ -18,15 +56,15 @@ export default function Contact() {
         </Flex>
         <Flex flexDirection="column" gap="2.5rem">
           <Text variant="description-contact">{TEXT.DESCRIPTION}</Text>
-          <S.Form onSubmit={handleSubmit}>
+          <S.Form onSubmit={sendEmail}>
             <S.Inputs>
               <Flex flexDirection="column" width="100%">
                 <S.Label htmlFor="name">Nome</S.Label>
                 <S.Input
                   type="text"
-                  id="name"
-                  name="name"
                   placeholder="Digite seu nome"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
                   required
                 />
               </Flex>
@@ -34,8 +72,8 @@ export default function Contact() {
                 <S.Label htmlFor="email">E-mail</S.Label>
                 <S.Input
                   type="email"
-                  id="email"
-                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   placeholder="Digite seu e-mail"
                   required
                 />
@@ -47,12 +85,16 @@ export default function Contact() {
                 as="textarea"
                 id="message"
                 name="message"
+                value={message}
                 placeholder="Digite sua mensagem aqui"
+                onChange={(e) => setMessage(e.target.value)}
                 rows={4}
                 required
               ></S.Input>
             </Flex>
-            <S.Button type="submit">{TEXT.TEXT_BUTTON}</S.Button>
+            <S.Button type="submit" onSubmit={sendEmail}>
+              {TEXT.TEXT_BUTTON}
+            </S.Button>
           </S.Form>
         </Flex>
       </S.Section>
---
+++ src/components/micro/Spinner/styles.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  margin: 0px auto;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.7);
+  left: 0;
+  top: 0;
+  justify-content: center;
+  z-index: 1000;
+  
+  .lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 51px;
+    height: 51px;
+    margin: 6px;
+    border: 6px solid #cef;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #cef transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
