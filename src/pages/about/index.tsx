a5c52ebced86771e5b610b1ec09071d55525753d
Bruno dos Santos Faria <bruno.faria@compasso.com.br>
2024-01-24 17:19:03 -0300

    Logo added to page headers and translation of title names

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,8 @@
 import { About } from "@/layouts/About";
-import Head from "next/head";
 
 export default function AboutP({}) {
   return (
     <>
-      <Head>
-        <title>Sobre</title>
-      </Head>
       <About />
     </>
   );
